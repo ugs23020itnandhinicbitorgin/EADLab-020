f28c2d18653b1e6769d94a97b98de993afbab991
ugs23020itnandhinicbitorgin <ugs23020_it.nandhini@cbit.org.in>
2025-11-09 22:13:50 +0530

    Add files via upload

---
+++ props.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Child from './Child';
+
+const Props = () => {
+    return (
+        <div>
+
+            <Child name="John" email="john.123@gmail.com"> </Child>
+            <Child name="John" email="john.123@gmail.com"> </Child>
+            <Child name="John" email="john.123@gmail.com"> </Child>
+              
+        </div>
+    );
+}
+export default Props;
